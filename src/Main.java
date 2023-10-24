5640f8225d5713d4ab6faa220af2daca900b6ee0
stepan TeSt <stepan.prosin@mail.ru>
2023-10-24 16:59:54 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticetCost = 25341;
+        int mileCost = 20;
+        int bonus = ticetCost / mileCost;
+
+        System.out.println("Количество бонусных милей полученых с покупки билета: " + bonus);
+    }
+}
